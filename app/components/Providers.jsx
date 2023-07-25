583d10544384289dfa48d7998acd8508601fef97
Rhythm Shandlya <armaanbgp@gmail.com>
2023-07-25 05:45:10 +0530

    .

--- app/components/Providers.jsx
+++
@@ -1,8 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-import React from "react";
-
-function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
-
-export default Providers;
--- app/components/text/textBox.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const textBox = ({ text }) => {
-  return <>{}</>;
-};
-
-export default textBolt;
